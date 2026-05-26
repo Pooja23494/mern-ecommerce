77fef206d30932971002fbb7fd7279b6c5da6e67
Pooja Patel <poojaclass1994@gmail.com>
2026-05-26 10:45:37 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-ecommerce-seven-nu.vercel.app",
     credentials: true,
   }),
 );
